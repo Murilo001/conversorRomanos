bde72f1d73a096ca8704c9dde69d3226c2e6802b
Murilo001 <murilo001os@gmail.com>
2019-09-22 23:45:00 -0300

    Hello world F#

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let convertNumber numero = printfn "Converti %s pra string" numero
+
+[<EntryPoint>]
+let main argv =
+    convertNumber "Argument Number"
+    //printfn "Hello World from F#!"
+    0 // return an integer exit code
